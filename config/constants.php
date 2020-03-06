13526461c30a3373ba0d954ecbedd086dc3c6718
Alvinditya Saputra <alvindityas@gmail.com>
2020-03-06 22:33:35 +0700

    add constant value

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'FACTORY' => [
+		'CREATE' => 'create',
+		'UPDATE' => 'update'
+	],
+
+	'USER_ROLE_CHOICE' => 'ADMINISTRATOR,OPS',
+	'USER_ROLE' => [
+		'ADMINISTRATOR' => 'ADMINISTRATOR',
+		'OPS' => 'OPS'
+	]
+];
